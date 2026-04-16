8712876e162fd065889d7fd194b0eb4378c427e9
eunseo121777 <eunseo121777@naver.com>
2026-04-16 14:08:50 +0900

    modified:   Lotto.py

--- Lotto.py
+++ Lotto.py
@@ -8,5 +8,7 @@ for i in range (5):
     while a in lotto_list:
         a=int(random.randint(1,45))
     lotto_list.append(a)
-
-print("추첨된 로또 번호 ==>",lotto_list[0],lotto_list[1],lotto_list[2],lotto_list[3],lotto_list[4],lotto_list[5])
+lotto_list.sort()
+print("추첨된 로또 번호 ==> ",end='')
+for i in range(6):
+    print("%d "%lotto_list[i],end='')
