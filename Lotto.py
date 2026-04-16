bf8370cf9e0cf618aca21671b30cc6347b7b0018
eunseo121777 <eunseo121777@naver.com>
2026-04-16 12:40:07 +0900

    modified:   Lotto.py

--- Lotto.py
+++ Lotto.py
@@ -1,11 +1,12 @@
 import random
 lotto_list=[]
-a=0
-print("로또 추첨을 시작합니다.")
-for i in range (6):
+a=int(random.randint(1,45))
+lotto_list.append(a)
+print("** 로또 추첨을 시작합니다. **")
+for i in range (5):
     
     while a in lotto_list:
         a=int(random.randint(1,45))
     lotto_list.append(a)
 
-print("추첨된 로또 번호 ==>",lotto_list)
+print("추첨된 로또 번호 ==>",lotto_list[0],lotto_list[1],lotto_list[2],lotto_list[3],lotto_list[4],lotto_list[5])
---
+++ TurtleWrite.py
@@ -0,0 +1,4 @@
+import turtle as t
+import random
+a=t.textinput("문자열 입력","거북이 쓸 문자열을 입력")
+              
